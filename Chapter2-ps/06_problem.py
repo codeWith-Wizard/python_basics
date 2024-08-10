0375e6cc8b759494ad9cbb7f67a31d731cf7afdf
codeWith-Wizard <cosmicbyte.wizard@gmail.com>
2024-08-10 15:50:22 +0530

    initial commit

--- Chapter2-ps/06_problem.py
+++ Chapter2-ps/06_problem.py
@@ -1,5 +1,5 @@
 #program to find the square of the no. enetred by the user
 
 no1 = int(input("enter the no. : "));
-sqr = no1**2;
+sqr = no1**2;  #in c++ we use no1^2
 print("square of ",no1," is :",sqr);
