c5a61757b4ed4549d326982507db9d537cc2af22
codeWith-Wizard <cosmicbyte.wizard@gmail.com>
2024-08-11 14:46:23 +0530

    initial commit

---
+++ Chapter4-ps/03_problem.py
@@ -0,0 +1,8 @@
+#sum of no.s in list 
+a=[1,2,3,4];
+
+print(sum(a));
+
+#b = ["a", "b", "c"];  
+#print(sum(b));
+'''string list cant use sum function'''
---
+++ Chapter4-ps/04-problem.py
@@ -0,0 +1,5 @@
+#counting 0s in tuple
+
+a = (1,2,0,756,56,0,4,3,2,43,40,0);
+
+print(a.count(0));
