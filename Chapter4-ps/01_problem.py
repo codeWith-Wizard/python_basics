73913c7dd0e26f0e87cd90b049f693f61b83d697
codeWith-Wizard <cosmicbyte.wizard@gmail.com>
2024-08-11 14:41:13 +0530

    intial commit

---
+++ Chapter4-ps/01_problem.py
@@ -0,0 +1,7 @@
+#program to store seven fruit name in the list entered by the user
+
+fruits =[];
+for i in range(8):
+    fruits.append(input("enter fruit name : ")) 
+
+print(fruits);
---
+++ Chapter4-ps/02_problem.py
@@ -0,0 +1,9 @@
+#accepting marks of 6 students
+
+marks = [];
+
+for i in range(7):
+    marks.append(input("enetr the marks of student : "))
+
+for i in range (7):
+    print("marks of student", i+1 , " : ", marks[i])
