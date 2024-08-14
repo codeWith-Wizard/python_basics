d121abbd5bdbf262b475acfb3c4590c3cee5a0bf
codeWith-Wizard <cosmicbyte.wizard@gmail.com>
2024-08-14 20:02:36 +0530

    initial commit

---
+++ Chapter5/03_sets.py
@@ -0,0 +1,17 @@
+#using sets
+
+a = {} #empty dictionary
+s = {1,2,3,4} #set
+my_set = set([1,2,3,4,4,5,5,23,2]) #will only take unique elements
+
+'''creating empty set'''
+e = set(); #empty set
+
+
+print(type(a));
+print(type(s));
+print(type(my_set));
+print(type(e))
+
+print(s);
+print(my_set);
