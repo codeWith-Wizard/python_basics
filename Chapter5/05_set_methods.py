57d4750aefbd6ae774e974a16daf699a52b64137
codeWith-Wizard <cosmicbyte.wizard@gmail.com>
2024-08-14 20:58:24 +0530

    initial commit

---
+++ Chapter5/05_set_methods.py
@@ -0,0 +1,15 @@
+#using different methods of set
+a = {1,2,3,45,56,32,23,45}
+
+#1 len method
+length_a = len(a);
+print(length_a);
+
+#2 remove method => used to rmeove particular element
+a.remove(3);
+print(a);
+
+#3 pop method use to delete and retrun random element
+a.pop();
+print(a);
+'''not typically used'''
---
+++ Chapter5/06_maths_sets.py
@@ -0,0 +1,21 @@
+#using union , intersection and other things on sets just like maths
+
+s1 = {1,2,3,4,5,6};
+s2 = {3,45,4,1,78};
+
+#UNION 
+s3 = s1.union(s2);
+print(s3, "and " , type(s3));
+
+#INTERSECTION
+s4 = s1.intersection(s2);
+print(s4);
+
+print(s1-s2);
+print(s1);
+
+
+
+
+
+
