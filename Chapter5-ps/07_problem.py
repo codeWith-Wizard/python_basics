be1af2842f8a4d2713f3c1e6d88fde8a9a71cec5
codeWith-Wizard <cosmicbyte.wizard@gmail.com>
2024-08-15 12:26:13 +0530

    initial commit

---
+++ Chapter5-ps/07_problem.py
@@ -0,0 +1,7 @@
+'''can we cahnge the value of list contained in set'''
+
+a =[1,2];
+a_set = {4,7,"jaggu",[1,2]};  #returns error as list cannot be containe in set
+print(a_set);
+
+'''lists are mutable and not hashable'''
